337985edca9b3d9c968f19f4639f67bafc1aa96f
Marc Espin Sanz <mespinsanz@gmail.com>
2022-02-25 14:05:45 +0100

    :tada: Init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@fontsource/inter";
+import "@fontsource/inter/800.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
